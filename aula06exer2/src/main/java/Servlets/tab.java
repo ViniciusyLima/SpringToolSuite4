be2d64e99e3358eaf8d525a1a1058c2dd4c90e05
Auto Logon <autologon@CSED.LAB>
2023-09-13 10:52:39 -0300

    aula06exer2

---
+++ aula06exer2/src/main/java/Servlets/tab.java
@@ -0,0 +1,30 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/tab")
+public class tab extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int numero = Integer.parseInt(request.getParameter("valor"));
+		PrintWriter out = response.getWriter();
+		for (int i=0;i<=10;i++) {
+			out.println(i + " x 5 = " + i * numero);
+		}
+	}
+
+}
--- aula06exer3/src/main/java/Servlets/calc.java
+++ aula06exer3/src/main/java/Servlets/calc.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Servlet")
-public class Servlet extends HttpServlet {
+
+@WebServlet("/calc")
+public class calc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,19 +20,12 @@ public class Servlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String user = "Vanderson";
-		String pass = "1234";
-		String nome = request.getParameter("usuario");
-		String senha = request.getParameter("senha");
-		if (nome.equals(user)&& pass.equals(senha)) {
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("Parabéns, senha Correta!");
-		}else {
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("Usuário ou senha invalidos!");
-		}
+		int sal = 1000;
+		int salFunc = Integer.parseInt(request.getParameter("valor"));
+		double qtdSalario = salFunc/sal;
+		PrintWriter out = response.getWriter();
+		out.println(" ");
+		out.println("A quantidade de Sálario: " + qtdSalario);
 	}
 
 }
