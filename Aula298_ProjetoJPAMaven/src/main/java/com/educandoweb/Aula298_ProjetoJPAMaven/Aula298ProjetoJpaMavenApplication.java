805b2f1fe6a0043ba16b98a017023a446e546b3f
unknown <viniuciusyl@hotmail.com>
2023-12-16 12:51:04 -0300

    Iniciando o Projeto JPA/HIBERNATE usando Maven

---
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/Aula298ProjetoJpaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Aula298_ProjetoJPAMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula298ProjetoJpaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula298ProjetoJpaMavenApplication.class, args);
+	}
+
+}
---
+++ Aula298_ProjetoJPAMaven/src/test/java/com/educandoweb/Aula298_ProjetoJPAMaven/Aula298ProjetoJpaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Aula298_ProjetoJPAMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula298ProjetoJpaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
