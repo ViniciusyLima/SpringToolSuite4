b0de5c465d1dc503c3dd5da95c36991c83c79b2a
unknown <viniuciusyl@hotmail.com>
2023-12-16 17:56:11 -0300

    Product

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/Category.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.Aula298_ProjetoJPAMaven.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,6 +21,9 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -42,6 +48,10 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
---
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.Aula298_ProjetoJPAMaven.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Product;
+import com.educandoweb.Aula298_ProjetoJPAMaven.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
