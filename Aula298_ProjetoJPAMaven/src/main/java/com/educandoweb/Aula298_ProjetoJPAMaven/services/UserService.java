7da1a9de8e347d9ac0d95daa38a091386d81a41e
unknown <viniuciusyl@hotmail.com>
2023-12-16 15:13:42 -0300

    Service layer, component registration

---
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.Aula298_ProjetoJPAMaven.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.Aula298_ProjetoJPAMaven.entities.User;
+import com.educandoweb.Aula298_ProjetoJPAMaven.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
