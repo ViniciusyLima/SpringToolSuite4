0bd08e5c7ba6b0affb06e7e31b7ecce6ddab8ea8
unknown <viniuciusyl@hotmail.com>
2023-12-18 21:36:51 -0300

    ManyToMany associação

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/Product.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/Product.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -24,13 +27,14 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 	}
-	
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
