a58aa3a44ee6d3ca13215ba7db2a1526eb1303d5
unknown <viniuciusyl@hotmail.com>
2024-01-11 19:22:47 -0300

    OrderItem 1/2

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/pk/OrderItemPK.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/pk/OrderItemPK.java
@@ -1,11 +1,25 @@
 package com.educandoweb.Aula298_ProjetoJPAMaven.entities.pk;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Order;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Product;
 
-public class OrderItemPK {
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Embeddable
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
 	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
 	private Product product;
 	
 	public Order getOrder() {
@@ -21,5 +35,20 @@ public class OrderItemPK {
 		this.product = product;
 	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
 }
