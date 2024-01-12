d358ff9aa8ec9c34b3d13eae37a9275719413a8f
unknown <viniuciusyl@hotmail.com>
2024-01-12 18:04:24 -0300

    Payment OneToOne

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/config/TestConfig.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Category;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Order;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.OrderItem;
+import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Payment;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.Product;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.User;
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.enums.OrderStatus;
@@ -80,5 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
