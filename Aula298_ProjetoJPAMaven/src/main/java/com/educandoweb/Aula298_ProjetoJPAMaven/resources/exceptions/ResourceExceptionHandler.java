bdb0727b02c63e961978825608e2a67ac38c1527
unknown <viniuciusyl@hotmail.com>
2024-01-12 19:27:36 -0300

    Exception handling

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/resources/exceptions/ResourceExceptionHandler.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.Aula298_ProjetoJPAMaven.services.exceptions.DatabaseException;
 import com.educandoweb.Aula298_ProjetoJPAMaven.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourcenotfound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value() , error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value() , error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/services/UserService.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.Aula298_ProjetoJPAMaven.entities.User;
 import com.educandoweb.Aula298_ProjetoJPAMaven.repositories.UserRepository;
+import com.educandoweb.Aula298_ProjetoJPAMaven.services.exceptions.DatabaseException;
 import com.educandoweb.Aula298_ProjetoJPAMaven.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
