5330ec5dab904f2414597c762cbb71c5bdbc76bc
unknown <viniuciusyl@hotmail.com>
2024-01-12 18:13:31 -0300

    Subtotal e Total methods

--- Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/OrderItem.java
+++ Aula298_ProjetoJPAMaven/src/main/java/com/educandoweb/Aula298_ProjetoJPAMaven/entities/OrderItem.java
@@ -70,6 +70,10 @@ public class OrderItem implements Serializable{
 		return Objects.hash(id);
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
