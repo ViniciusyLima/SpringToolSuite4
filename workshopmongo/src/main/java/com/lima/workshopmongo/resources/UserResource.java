1c9e6810940e3b93d37000b242e42651b0b65cdd
unknown <viniuciusyl@hotmail.com>
2024-01-19 16:16:56 -0300

    Standard e Handler Exception

--- workshopmongo/src/main/java/com/lima/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/lima/workshopmongo/resources/UserResource.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lima.workshopmongo.domain.User;
@@ -20,10 +21,16 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- workshopmongo/src/main/java/com/lima/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/lima/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lima.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lima.workshopmongo.domain.User;
 import com.lima.workshopmongo.repository.UserRepository;
+import com.lima.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.get();
 	}
 }
